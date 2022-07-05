750681033dd728d00ac1a66ade19026306690d20
MasahideOkada <masahideokada1121@gmail.com>
2022-07-05 23:39:47 +0900

    made write_note

--- make_note.py
+++ make_note.py
@@ -2,14 +2,23 @@ import math
 import numpy as np
 from scipy.io.wavfile import write
 
-sample_rate = 44100
-duration = 1.0
+note_names = [
+    'C', 'CsDb', 'D', 'DsEb', 'E', 'F', 'FsGb', 'G', 'GsAb', 'A', 'AsBb', 'B' 
+]
+
+def write_note(noteid, A4_freq=440, octave=0, duration=1.0, samplerate=44100):
+    distance_from_A = noteid - 9
+    note_freq = A4_freq * math.pow(2, distance_from_A / 12) * math.pow(2, octave)
+    time_array = np.arange(samplerate * duration)
+    ft = note_freq * time_array
+    sinwave = np.sin(2 * np.pi * ft / samplerate)
+    sinwave = np.int16(sinwave * 32767)
+    notename = note_names[noteid]
+    file_name = f'notes/{notename}{4+octave}inA{A4_freq}.wav'
+    write(file_name, samplerate, sinwave)
+
+duration = 0.8
 A4_freq = 440
-octave = 0
-note_freq = A4_freq * math.pow(2, octave)
-time_array = np.arange(sample_rate * duration)
-ft = np.int64(note_freq * time_array)
-sinwave = np.sin(2 * np.pi * ft / sample_rate)
-sinwave = np.int16(sinwave * 32767)
-file_name = 'notes/A440.wav'
-write(file_name, sample_rate, sinwave)
+for octave in range(-2, 2):
+    for noteid in range(12):
+        write_note(noteid, A4_freq, octave, duration) 
